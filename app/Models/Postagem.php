d43836c052fc45fe2ced6d0fa77988c33b968c7d
Gustavo Oliveira <gustavooliveiracf3@gmail.com>
2025-04-06 16:07:46 -0300

    .

--- app/Models/Postagem.php
+++ app/Models/Postagem.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasOne;
 class Postagem extends Model
 {
     protected $table= 'postagens';
