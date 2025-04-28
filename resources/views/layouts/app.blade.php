1ccc3cc0877263c70fd0484e22b3c5b055391e57
Gustavo Oliveira <gustavooliveiracf3@gmail.com>
2025-04-27 21:49:10 -0300

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 		<title>Dopetrope by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="{{ url ("assets/css/main.css") }}" />
 	</head>
 	<body class="homepage is-preload">
 		<div id="page-wrapper">
@@ -189,12 +189,12 @@
 		</div>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+			<script src="{{ url ("assets/js/jquery.min.js") }}"></script>
+			<script src="{{ url ("assets/js/jquery.dropotron.min.js") }}"></script>
+			<script src="{{ url ("assets/js/browser.min.js") }}"></script>
+			<script src="{{ url ("assets/js/breakpoints.min.js") }}"></script>
+			<script src="{{ url ("assets/js/util.js") }}"></script>
+			<script src="{{ url ("assets/js/main.js") }}"></script>
 
 	</body>
 </html>
