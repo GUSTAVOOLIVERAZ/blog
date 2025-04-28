3a3b7276fc4e1a017acfacce9d4ae2fc54d4cf5c
Gustavo Oliveira <gustavooliveiracf3@gmail.com>
2025-04-28 20:55:02 -0300

    .

--- config/adminlte.php
+++ config/adminlte.php
@@ -339,8 +339,8 @@ return [
             'icon' => 'fas fa-fw fa-user',
         ],
         [
-            'text' => 'change_password',
-            'url' => 'admin/settings',
+            'text' => 'Alterar senha',
+            'url' => 'admin/alterarSenha',
             'icon' => 'fas fa-fw fa-lock',
         ],
         [
