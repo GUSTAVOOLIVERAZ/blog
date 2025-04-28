546eafa0ba076a75a83c8fc21358de344e770bc3
Gustavo Oliveira <gustavooliveiracf3@gmail.com>
2025-04-27 21:20:04 -0300

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,10 @@
 
 <html>
     <head>
-        <title>App Name - @yield('title')</title>
+        <title>Teste</title>
     </head>
     <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
+      
 
         <div class="container">
             @yield('content')
