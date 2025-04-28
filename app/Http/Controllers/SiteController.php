7e04eb44b919992af4dc97882e6f60c6d16a9652
Gustavo Oliveira <gustavooliveiracf3@gmail.com>
2025-04-27 22:16:11 -0300

    .

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class SiteController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $categorias = Categorias::orderBy('nome', 'ASC')->get();
+        return view('welcome', compact('categorias'));
+
+
+        //return view('site.index');
     }
     
 }
