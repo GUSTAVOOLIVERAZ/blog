688562424f987450f5825e3090a31104135ec2ad
Gustavo Oliveira <gustavooliveiracf3@gmail.com>
2025-04-28 21:02:25 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -19,43 +19,49 @@ Route::get('/PostagemByCategoriaId/{id}', [SiteController::class, 'PostagemByCat
 Route::get('/PostagemByAutorId/{id}', [SiteController::class, 'PostagemByAutorId'])->name('site.PostagemByAutorId');
 
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// -----------------------------------------------------------------------------
-// CATEGORIA
-// -----------------------------------------------------------------------------
-
-Route::get('/categoria', [CategoriaController::class, 'index'])->name('categoria.index');
-
-Route::get('/categoria/create', [CategoriaController::class, 'create'])->name('categoria.create');
-
-Route::post('/categoria', [CategoriaController::class, 'store'])->name('categoria.store');
-
-Route::get('/categoria/{id}', [CategoriaController::class, 'show'])->name('categoria.show');
-
-Route::get('/categoria/{id}/edit', [CategoriaController::class, 'edit'])->name('categoria.edit');
-
-Route::put('/categoria/{id}', [CategoriaController::class, 'update'])->name('categoria.update');
-
-Route::delete('/categoria/{id}', [CategoriaController::class, 'destroy'])->name('categoria.destroy');
+    Auth::routes();
+    Route::middleware(['auth'])->group(function () {
 
 
-// -----------------------------------------------------------------------------
-// POSTAGEM
-// -----------------------------------------------------------------------------
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/postagem', [PostagemController::class, 'index'])->name('postagem.index');
+        // -----------------------------------------------------------------------------
+        // CATEGORIA
+        // -----------------------------------------------------------------------------
 
-Route::get('/postagem/create', [PostagemController::class, 'create'])->name('postagem.create');
+        Route::get('/categoria', [CategoriaController::class, 'index'])->name('categoria.index');
 
-Route::post('/postagem', [PostagemController::class, 'store'])->name('postagem.store');
+        Route::get('/categoria/create', [CategoriaController::class, 'create'])->name('categoria.create');
 
-Route::get('/postagem/{id}', [PostagemController::class, 'show'])->name('postagem.show');
+        Route::post('/categoria', [CategoriaController::class, 'store'])->name('categoria.store');
 
-Route::get('/postagem/{id}/edit', [PostagemController::class, 'edit'])->name('postagem.edit');
+        Route::get('/categoria/{id}', [CategoriaController::class, 'show'])->name('categoria.show');
 
-Route::put('/postagem/{id}', [PostagemController::class, 'update'])->name('postagem.update');
+        Route::get('/categoria/{id}/edit', [CategoriaController::class, 'edit'])->name('categoria.edit');
 
-Route::delete('/postagem/{id}', [PostagemController::class, 'destroy'])->name('postagem.destroy');
+        Route::put('/categoria/{id}', [CategoriaController::class, 'update'])->name('categoria.update');
+
+        Route::delete('/categoria/{id}', [CategoriaController::class, 'destroy'])->name('categoria.destroy');
+
+
+        // -----------------------------------------------------------------------------
+        // POSTAGEM
+        // -----------------------------------------------------------------------------
+
+        Route::get('/postagem', [PostagemController::class, 'index'])->name('postagem.index');
+
+        Route::get('/postagem/create', [PostagemController::class, 'create'])->name('postagem.create');
+
+        Route::post('/postagem', [PostagemController::class, 'store'])->name('postagem.store');
+
+        Route::get('/postagem/{id}', [PostagemController::class, 'show'])->name('postagem.show');
+
+        Route::get('/postagem/{id}/edit', [PostagemController::class, 'edit'])->name('postagem.edit');
+
+        Route::put('/postagem/{id}', [PostagemController::class, 'update'])->name('postagem.update');
+
+        Route::delete('/postagem/{id}', [PostagemController::class, 'destroy'])->name('postagem.destroy');
+
+        // -----------------------------------------------------------------------------[
+
+    });
